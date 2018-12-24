857e849bd2bcdcf3d913635cdccbed175bb74324
aska21 <askafaleh.af@gmail.com>
2018-12-24 13:52:13 +0700

    revisi

---
+++ application/views/template_admin_v2/dashboard_utama.php
@@ -0,0 +1,56 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <div class="row">
+                    <div class="col-md-9">
+                        
+                    </div>
+                    <div class="col-md-3">
+                       
+                    </div>
+                </div>
+            </div>
+            <div class="ibox-content">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="bxslider">
+                            <div style="max-height: 10px"><img src="<?=base_url()?>assets/images/u.jpg"></div>
+                            <div><img src="<?=base_url()?>assets/images/u2.jpg"></div>
+                            <div><img src="<?=base_url()?>assets/images/u3.jpg"></div>
+                            <div><img src="<?=base_url()?>assets/images/u4.jpg"></div>
+                            <div><img src="<?=base_url()?>assets/images/u1.jpg"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <h2>SMK TADIKA MESRA</h2>
+                        <hr/>
+                        <table class="table table-striped">
+                            <tr>
+                                <td>Alamat Sekolah </td>
+                                <td>:</td>
+                                <td>KAMPUNG DURIAN RUNTUH</td>
+                            </tr>
+                            <tr>
+                                <td>Kecamatan </td>
+                                <td>:</td>
+                                <td>DURIAN RUNTUH</td>
+                            </tr>
+                            <tr>
+                                <td>Kabupaten </td>
+                                <td>:</td>
+                                <td>SELANGOR</td>
+                            </tr>
+                            <tr>
+                                <td>Provinsi </td>
+                                <td>:</td>
+                                <td>KUALALUMPUR</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</form>
